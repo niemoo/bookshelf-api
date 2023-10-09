fb81e126426f2ed122377cf365c6685d7a26772e
Izzan Abdul <izzanabdul123@gmail.com>
2023-10-09 16:17:00 +0700

    feat: add + fixing func
    add func getAllBooksHandler, getBookByIdHandler, and fixing status name in func addBooksHandler.

--- src/handler.js
+++ src/handler.js
@@ -1,16 +1,33 @@
 const { nanoid } = require('nanoid');
 const books = require('./books');
 
+// FUNC ADD NOTES WITH METHOD POST
 const addBooksHandler = (req, h) => {
+  // GET BODY REQUEST
   const {
     name,
     year,
     author,
     summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
   } = req.payload;
 
+  if (!name || name.trim() === '') {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Nama buku tidak boleh kosong.',
+    });
+
+    response.code(400);
+    return response;
+  }
+
+  // GET VALUE FOR id, finished, insertedAt, updatedAt
   const id = nanoid(16);
-  // const finished = false;
+  const finished = pageCount === readPage;
   const insertedAt = new Date().toISOString();
   const updatedAt = insertedAt;
 
@@ -19,7 +36,12 @@ const addBooksHandler = (req, h) => {
     year,
     author,
     summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
     id,
+    finished,
     insertedAt,
     updatedAt,
   };
@@ -28,10 +50,11 @@ const addBooksHandler = (req, h) => {
 
   const isSuccess = books.filter((book) => book.id === id);
 
+  // IF SUCCESS ADD BOOKS
   if (isSuccess) {
     const response = h.response({
       status: 'success',
-      message: 'Book added successfully',
+      message: 'Buku berhasil ditambahkan',
       data: {
         bookId: id,
       },
@@ -41,13 +64,56 @@ const addBooksHandler = (req, h) => {
     return response;
   }
 
+  // Use an array of error messages for better readability
+  // const errorMessages = [];
+  // if (!name || name.trim() === '') {
+  //   errorMessages.push('Gagal menambahkan buku. Nama buku tidak boleh kosong.');
+  // }
+  // if (readPage > pageCount) {
+  //   errorMessages.push('Gagal menambahkan buku. readPage
+  // tidak boleh lebih besar dari pageCount.');
+  // }
+
+  // IF FAIL ADD BOOKS
   const response = h.response({
     status: 'fail',
-    message: 'Book failed to add',
+    message: 'Gagal menambahkan buku.',
   });
 
   response.code(400);
   return response;
 };
 
-module.exports = { addBooksHandler };
+// FUNC FOR SHOW ALL BOOKS WITH METHOD GET
+const getAllBooksHandler = () => ({
+  status: 'success',
+  data: {
+    books,
+  },
+});
+
+// FUNC SHOW BOOK BY ID WITH METHOD GET
+const getBookByIdHandler = (req, h) => {
+  const { bookId } = req.params;
+
+  const book = books.filter((buku) => buku.id === bookId)[0];
+
+  if (book !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        book,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan',
+  });
+
+  response.code(404);
+  return response;
+};
+
+module.exports = { addBooksHandler, getAllBooksHandler, getBookByIdHandler };
